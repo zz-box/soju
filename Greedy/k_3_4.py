cf7fea386a4507767cf7dbd798e61cad84f972b4
bondyuu <leo4902@gmail.com>
2022-09-08 20:42:07 +0900

    Create k_3_4.py

---
+++ Greedy/k_3_4.py
@@ -0,0 +1,15 @@
+n, m = map(int, input().split())
+
+cnt = 0
+while n >= m:
+    if n % m == 0:
+        n = n//m
+        cnt += 1
+    else:
+        n -= 1
+        cnt += 1
+
+if n > 1:
+    cnt += (n - 1)
+print(25//5)
+print(cnt)
