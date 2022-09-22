6b462c6b820741c0b561408b7d172885a25dc171
bondyuu <leo4902@gmail.com>
2022-09-22 10:59:42 +0900

    본주_1번 에러 수정

--- DFS,BFS/k_5_1.py
+++ DFS,BFS/k_5_1.py
@@ -1,28 +1,28 @@
 n, m = map(int, input().split())
 
 graph = []
-for i in range(m):
-    graph.append(list(map(int, input().split())))
+for i in range(n):
+    graph.append(list(map(int, input())))
 
-def function(x,y):
+print(graph)
+def func(x,y):
     if x <= -1 or x >= n  or y <= -1 or y>=m:
         return False
     
     if graph[x][y] == 0:
         graph[x][y] = 1
         
-        function[x-1][y]
-        function[x+1][y]
-        function[x][y-1]
-        function[x][y+1]
-        
+        func(x-1, y)
+        func(x+1, y)
+        func(x, y-1)
+        func(x, y+1)
         return True
     return False
 
 result = 0
 for i in range(n):
     for j in range(m):
-        if function(n,m):
+        if func(i,j) == True:
             result += 1
 
 print(result)
